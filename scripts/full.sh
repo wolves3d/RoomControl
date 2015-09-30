4be301ef4e3021fbfb8eaf82cc9949184df4f8ae
root <wolves3d@gmail.com>
2015-10-01 05:14:08 +1000

    script changed

--- scripts/full.sh
+++ scripts/full.sh
@@ -3,14 +3,20 @@
 USER="sudo -H -u linuxium"
 ROOT="sudo"
 
+clear
+
 # pull sources -----------------------------------------------------------------
+echo "Updating sources"
 $USER git pull
 if [ $? -ne 0 ]; then
     echo "git pull failed"
     exit 1
 fi
+echo "\n"
+
 
 # build client -----------------------------------------------------------------
+echo "Building client"
 cd ../client
 $USER make clean
 $USER make
@@ -18,6 +24,8 @@ if [ $? -ne 0 ]; then
     echo "client build failed"
     exit 1
 fi
+echo "\n"
+
 
 # build arduino sketch ---------------------------------------------------------
 cd ../arduino
