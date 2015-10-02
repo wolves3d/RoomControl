6f6757958ae89febcc1bb406d8177fc46e1b9c57
Vladimir Kovner <wolves3d@gmail.com>
2015-10-02 03:48:39 +0300

    gcc compilation

--- source/main.cpp
+++ source/main.cpp
@@ -147,3 +147,19 @@ int main()
 //	comPort.Close();
 	return 0;
 }
+
+
+
+#ifndef WIN32
+
+#include <sys/time.h>
+unsigned GetTickCount()
+{
+	struct timeval tv;
+	if (gettimeofday(&tv, NULL) != 0)
+		return 0;
+
+	return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+}
+
+#endif
